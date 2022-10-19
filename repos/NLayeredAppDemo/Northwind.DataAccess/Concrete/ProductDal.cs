3a6257b2120b98084e23685770d9b3e11c96f645
Muhammet Çelik <muhammetclk56@gmail.com>
2022-10-19 14:16:17 +0300

    DataAcces katmaninda contextimizi olusturduk ve NuGet Packages ile EntityFrameworku ekledik

--- repos/NLayeredAppDemo/Northwind.DataAccess/Concrete/ProductDal.cs
+++ repos/NLayeredAppDemo/Northwind.DataAccess/Concrete/ProductDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Northwind.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace Northwind.DataAccess.Concrete
 {
     class ProductDal
     {
+        public List<Product> GetAll()
+        {
+
+
+        }
+        public List<Product> Get(int id)
+        {
+
+        }
+        public void Add(Product product)
+        {
+
+
+        }
+        public void Update(Product product)
+        {
+
+
+        }
+        public void Delete(Product product)
+        {
+
+
+        }
     }
 }
