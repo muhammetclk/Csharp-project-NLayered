bc08130d9a562e35faf142f21cfaf0993c3dcb0c
Muhammet Çelik <muhammetclk56@gmail.com>
2022-10-19 14:19:49 +0300

    Product icin olan contextimizi kullanabilmek icin Dbset ettik.

--- repos/NLayeredAppDemo/Northwind.DataAccess/Concrete/NorthwindContext.cs
+++ repos/NLayeredAppDemo/Northwind.DataAccess/Concrete/NorthwindContext.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using Northwind.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Northwind.DataAccess.Concrete
 {
-    public class NorthwindContext
+    public class NorthwindContext:DbContext
     {
-
+        public DbSet<Product> Products { get; set; }
     }
 }
